1baf174b3a232c960141ceeb78c1f415a85eb489
zplato <zach.plato@gmail.com>
2024-10-24 21:01:19 -0400

    add CORS and rest support for flask

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,6 +1,15 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_restful import Api
+from flask_cors import CORS
 
+
+# Flask Stuff
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+
+# Home Route
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
