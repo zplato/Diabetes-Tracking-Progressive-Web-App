2aeb729adf3b0bc6be4064620478c70ba16e9942
Anjeline Sayoc <anjeline.sayoc@gmail.com>
2024-11-13 23:07:30 +1100

    Update MyAchievements.js

--- frontend/src/components/MyAchievements.js
+++ frontend/src/components/MyAchievements.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, Typography, Card, CardContent } from '@mui/material';
-import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import StarsIcon from '@mui/icons-material/Stars';
 
 export function MyAchievements({ username = "John" }) {
   // Placeholder values for rank and points
@@ -9,33 +9,50 @@ export function MyAchievements({ username = "John" }) {
   const pointsToRankUp = 300;
 
   return (
-    <Box p={3} display="flex" flexDirection="column" alignItems="center">
-      {/* Greeting message with the username */}
-      <Typography variant="h5" mb={3}>
-        Hello, {username}. Good job, keep it up!
-      </Typography>
-      <Box display="flex" alignItems="center" width="100%" maxWidth={600}>
-        {/* Trophy icon to the left of the achievement information */}
-        <EmojiEventsIcon sx={{ fontSize: 100, mr: 4 }} />
-        <Card sx={{ flexGrow: 1 }}>
+    <Box p={3} display="flex" justifyContent="center" alignItems="center">      
+      {/* Kid 1 with trophy */}
+      <Box component="img" src="/images/kid-trophy-1.jpg" alt="doctor-image" sx={{ width: '170px' }} />
+      
+      <Box display="flex" alignItems="center" flexDirection="column" width="100%" maxWidth={600}>
+        {/* Greeting message with the username */}
+        <Typography mt={1} mb={2} sx={{ fontSize: '24px', letterSpacing: '0.7px', color: 'black' }}>
+          Hey, {username}!
+        </Typography>
+        <Typography mb={4} sx={{ fontSize: '24px', letterSpacing: '0.7px', color: 'black' }}>
+          Good job! Keep it up~
+        </Typography>        
+        <Card sx={{ flexGrow: 1, bgcolor: '#EDFFE0', p: 2}}>
           <CardContent>
             {/* Achievement information grid */}
-            <Box display="grid" gridTemplateColumns="1fr 1fr" gap={2}>
+            <Box display="grid" gridTemplateColumns="repeat(2, 1fr)" gap={3}>
               {/* Displaying user's rank */}
-              <Typography variant="h6" textAlign="left">Your Rank</Typography>
-              <Typography variant="body1" textAlign="right">{rank}</Typography>
+              <Box display="flex" alignItems="center">
+                <StarsIcon sx={{ mr: 1 , color: 'orange', pl: 3}} />
+                <Typography sx={{ fontSize: '18px', letterSpacing: '0.7px', color: '#333333' }}>Your Rank</Typography>
+              </Box>
+              <Typography sx={{ fontSize: '18px', letterSpacing: '0.7px', color: '#333333', pl: 4 }} textAlign="center">{rank}</Typography>
 
               {/* Displaying current points */}
-              <Typography variant="h6" textAlign="left">Current Points</Typography>
-              <Typography variant="body1" textAlign="right">{currentPoints}</Typography>
+              <Box display="flex" alignItems="center">
+                <StarsIcon sx={{ mr: 1 , color: 'orange', pl: 3}} />
+                <Typography sx={{ fontSize: '18px', letterSpacing: '0.7px', color: '#333333' }}>Current Points</Typography>
+              </Box>
+              <Typography sx={{ fontSize: '18px', letterSpacing: '0.7px', color: '#333333', pl: 4 }} textAlign="center">{currentPoints}</Typography>
 
               {/* Displaying points needed to rank up */}
-              <Typography variant="h6" textAlign="left">Points to Rank Up</Typography>
-              <Typography variant="body1" textAlign="right">{pointsToRankUp}</Typography>
+              <Box display="flex" alignItems="center">
+                <StarsIcon sx={{ mr: 1 , color: 'orange', pl: 3}} />
+                <Typography sx={{ fontSize: '18px', letterSpacing: '0.7px', color: '#333333' }}>Points to Rank Up</Typography>
+              </Box>
+              <Typography sx={{ fontSize: '18px', letterSpacing: '0.7px', color: '#333333', pl: 4 }} textAlign="center">{pointsToRankUp}</Typography>
+
             </Box>
           </CardContent>
         </Card>
-      </Box>
+      </Box>    
+
+      {/* Kid 2 with trophy */}
+      <Box component="img" src="/images/kid-trophy-2.jpg" alt="doctor-image" sx={{ width: '250px' }} />
     </Box>
   );
 }
