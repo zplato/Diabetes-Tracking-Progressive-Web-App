39a184f320ca3fdfcbb60305f230fab9a3afc0d9
Anjeline S. <130250636+asayoc@users.noreply.github.com>
2024-11-30 16:40:57 +1100

    Upload Database Artifacts Files

---
+++ Artifacts/Database/db.js
@@ -0,0 +1,4 @@
+app.config['MYSQL_DATABASE_HOST'] = 'cs6440-team62-db.c782iimocs6i.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_DATABASE_USER'] = 'admin'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'cs6440-db!'
+app.config['MYSQL_DATABASE_DB'] = 'cs6440-team62-db'
